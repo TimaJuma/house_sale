fd21cd0222fcb58ed41ef41e110ed92089ee4620
TimaJuma <tima.xpl@gmail.com>
2021-06-17 22:48:01 -0600

    initial setup

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,17 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import React, { ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface Iprops {
+  main: ReactNode;
+}
+
+const Layout: React.FC<Iprops> = ({ main }) => {
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      {main}
+    </div>
+  );
+};
+
+export default Layout;
